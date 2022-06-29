b286bce123c90460b622fd0f03edc068733e8fcd
Mathias Lauridsen <math227v@gmail.com>
2022-06-29 19:26:08 +0200

    :art: :sparkles: Structured main .ino file

--- tjommylygten/tjommylygten.ino
+++ tjommylygten/tjommylygten.ino
@@ -1,12 +1,38 @@
-#define FAN_POWER_PIN 'D8'
-#define FAN_PWM_PIN 'D5'
-#define LED_CONTROL_PIN 'D6'
-#define MODE_LED_PIN 'D13'
+#define LED_CONTROL_PIN 6
+#define FAN_POWER_PIN 8
+#define FAN_PWM_PIN 5
+#define MODE_LED_PIN 13
+#define SWITCH_PIN 9
+#define ENCODER_SW 10
+#define ENCODER_DT 11
+#define ENCODER_CLK 12
+
+#define LED_OFF 255
+#define FAN_OFF HIGH
 
 void setup() {
+  pinMode(LED_CONTROL_PIN, OUTPUT);
   pinMode(FAN_POWER_PIN, OUTPUT);
+  pinMode(FAN_PWM_PIN, OUTPUT);
+  pinMode(MODE_LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
+  pinMode(ENCODER_SW, INPUT);
+  pinMode(ENCODER_DT, INPUT);
+  pinMode(ENCODER_CLK, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read all pins
+  int mainSwitch = digitalRead(SWITCH_PIN);
+
+  // Do maths
+  // Fx calculate blink speed based on rotary encoder, etc.
+
+  // React on data
+  if ( mainSwitch == HIGH ) {
+
+  } else {
+    analogWrite(LED_CONTROL_PIN, LED_OFF);
+    digitalWrite(FAN_POWER_PIN, FAN_OFF);
+  }
 }
